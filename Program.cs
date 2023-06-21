44d7ff01283d503d5ad3f7e11d451ac8f149555b
KateYozz <koltunova@hotmail.com>
2023-06-21 23:27:03 +0300

    Initial commit: Added translation project for .resx files
    This commit includes the initial version of the project that translates .resx files from English to other languages using Azure Text Translation service. The project is set up to be easily configurable for different source files and target languages. This is the first step in making our software more accessible to users worldwide.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using Azure.AI.Translation.Text;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+using Azure;
+
+// Define your Azure Translation service credentials and file paths
+string subscriptionKey = "your_subscription_key";
+string subscriptionKey = "<your_subscription_key_here>";
+string sourceFilePath = "<path_to_your_source_file_here>";
+string targetFilePath = "<path_to_your_target_file_here>";
+string targetLanguage = "<target_language_here>";
+
+// Create an AzureKeyCredential object using your subscription key
+AzureKeyCredential credential = new(subscriptionKey);
+
+// Initialize the TextTranslationClient with your credentials
+TextTranslationClient translationService;
+
+try
+{
+    translationService = new TextTranslationClient(credential);
+}
+catch (UriFormatException)
+{
+    Console.WriteLine("The endpoint URL is not valid.");
+    return;
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("The subscription key is not valid.");
+    return;
+}
+
+// Check if the source file exists
+if (!File.Exists(sourceFilePath))
+{
+    Console.WriteLine($"Source file not found: {sourceFilePath}");
+    return;
+}
+
+// Load the source .resx file as an XDocument
+XDocument sourceDoc;
+try
+{
+    sourceDoc = XDocument.Load(sourceFilePath);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed to load the source file as an XML document: {ex.Message}");
+    return;
+}
+
+// Select all "data" elements that have a "value" child element
+var dataElements = sourceDoc.Descendants("data")
+                            .Where(x => x.Element("value") != null);
+
+if (!dataElements.Any())
+{
+    Console.WriteLine("The source file does not contain any translatable elements.");
+    return;
+}
+
+// Iterate over each "data" element
+foreach (var dataElement in dataElements)
+{
+    // Get the source text from the "value" element
+    string sourceText = dataElement.Element("value")?.Value;
+
+    if (sourceText == null)
+    {
+        continue;
+    }
+
+    // Translate the source text
+    try
+    {
+        // Create a new HtmlDocument and load the source text into it
+        HtmlDocument doc = new HtmlDocument();
+        doc.LoadHtml(sourceText);
+
+        // Iterate over each text node in the HtmlDocument
+        foreach (var textNode in doc.DocumentNode.DescendantsAndSelf().Where(n => n.NodeType == HtmlNodeType.Text))
+        {
+            string nodeText = textNode.InnerHtml;
+            var response = await translationService.TranslateAsync(targetLanguage, nodeText);
+            var result = response.Value.FirstOrDefault();
+            string translatedText = result?.Translations.FirstOrDefault()?.Text;
+
+            // Replace the text in the text node with the translated text
+            if (translatedText != null)
+            {
+                textNode.InnerHtml = translatedText;
+            }
+        }
+
+        // Get the translated HTML
+        string translatedHtml = doc.DocumentNode.OuterHtml;
+
+        // Replace the text in the "value" element with the translated text
+        dataElement.Element("value").Value = translatedHtml;
+
+        // Output the source text and the translated text
+        Console.WriteLine($"Source Text: {sourceText}");
+        Console.WriteLine($"Translated Text: {translatedHtml}");
+    }
+    catch (RequestFailedException ex)
+    {
+        Console.WriteLine($"Translation failed. Error code: {ex.ErrorCode}, Message: {ex.Message}");
+        return;
+    }
+}
+
+// Save the modified XDocument to the target file
+sourceDoc.Save(targetFilePath);
--- README.md
+++ README.md
@@ -19,23 +19,19 @@ TranslateResx is an open-source project that provides a solution for translating
 Subscription key is a unique identifier that is used to authenticate requests associated with your project for using Azure Cognitive Services, such as the Text Translation API. It's also known as an API key. Azure uses this key to debit your account according to the number of transactions made by your application.
 
 Here are the steps to get a subscription key for Azure Cognitive Services:
-
 Sign in to the Azure portal: If you don't have an Azure account, you'll need to create one.
-
 Create a resource: In the Azure portal, click on "Create a resource". Then, search for the service you want to use (in this case, "Translator Text"). Click on the service and then click "Create".
-
 Fill in the details: You'll need to fill in some details about your resource. You'll need to choose a name, subscription, resource group, pricing tier, and region. Once you've filled in all the details, click "Review + create" and then "Create".
-
 Get the key: Once your resource is created, go to "Resource Management" > "Keys and Endpoint". There you will find your key under "KEY1" or "KEY2". You can use either of these keys as your subscription key.
-
 Remember to keep your subscription key secure! Do not share your subscription key in your public GitHub repository. If your key is exposed, someone else could use it and incur charges on your account. Instead, you can instruct users to replace a placeholder text with their own subscription key in the code.
 
 Usage
+
 Set your Azure Cognitive Services subscription key and endpoint in the Program.cs file.
-Replace `your_subscription_key` in the code with your actual subscription key from Azure Cognitive Services.
-Configure your source File Path, for example "C:\\Development\\Actualog\\Actualog.Web.UI\\Resources\\Strings.resx"
-Add your target File Path, for example "C:\\Development\\Actualog\\Actualog.Web.UI\\Resources\\Strings.de.resx"
-Set target language code, for example "de"
+<your_subscription_key_here> should be replaced with your Azure Text Translation service subscription key.
+<path_to_your_source_file_here> should be replaced with the full path to your source .resx file. For example, it might be something like "C:\\Users\\YourName\\source\\repos\\YourProject\\Resources\\Strings.resx".
+<path_to_your_target_file_here> should be replaced with the full path where you want the translated .resx file to be saved. For example, it might be something like "C:\\Users\\YourName\\source\\repos\\YourProject\\Resources\\Strings.de.resx".
+<target_language_here> should be replaced with the language code for the language you want to translate to. For example, for German, it would be "de".
 
 Run the program with .NET CLI
 
